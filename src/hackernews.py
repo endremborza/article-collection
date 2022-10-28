bf3be1c7e3f7b322dc6663f4fe74bf52641f4dfc
Endre Márk Borza <endremborza@gmail.com>
2022-10-28 15:17:12 +0200

    change session breaking logic

--- src/hackernews.py
+++ src/hackernews.py
@@ -1,7 +1,15 @@
+from typing import Union
 import aswan
 from bs4 import BeautifulSoup
 
 main_url = "https://news.ycombinator.com/"
+
+
+class GiveUp(aswan.RequestHandler):
+    def is_session_broken(self, result: Union[int, Exception]):
+        if isinstance(result, int):
+            return result >= 500
+        return False
 
 
 class RegTop(aswan.RequestSoupHandler):
@@ -11,10 +19,12 @@ class RegTop(aswan.RequestSoupHandler):
             title_a = tr.find("a", class_="titlelink")
             if title_a is None:
                 title_a = tr.find("span", class_="titleline").find("a")
-            self.register_links_to_handler([title_a["href"]], aswan.RequestHandler)
+            self.register_links_to_handler([title_a["href"]], GiveUp)
 
         return soup.encode("utf-8")
 
 
 if __name__ == "__main__":
-    aswan.run_simple_project({RegTop: [main_url]}, name="hackernews", remote=True)
+    aswan.run_simple_project(
+        {RegTop: [main_url], GiveUp: []}, name="hackernews", remote=True
+    )
